4849a282d32c102ec9c1b3d4338011199083d008
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-27 09:23:22 +0100

    Finita la criptazione

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace CeasarCipher
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("inserisci una parola: ");
+            string plaintext = Console.ReadLine();
+            int key;
+            CaesarCode ml;
+
+            try
+            {
+                do
+                {
+                    Console.WriteLine("inserisci la chiave");
+                    key = int.Parse(Console.ReadLine());
+                } while (key < 0 || key > 25);
+
+                ml = new CaesarCode(plaintext, key);
+                Console.WriteLine($"Il testo cifrato è: {ml.CipherText}");
+
+            }
+            catch (Exception ex)
+            {
+
+
+                Console.WriteLine(ex.Message);
+               
+            }
+
+
+
+            Console.ReadLine();
         }
     }
 }
